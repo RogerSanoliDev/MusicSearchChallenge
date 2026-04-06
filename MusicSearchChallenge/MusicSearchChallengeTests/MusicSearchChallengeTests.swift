e7849b8daeeae556598a1deb0548d8810546e9aa
Roger Oliveira <rogersanoli.app@gmail.com>
2026-04-06 19:15:23 -0300

    [Challenge-001] Create project.
    This commit creates the initial XCode project.
    
    - Changes project to Swift 6
    - Adds App Icon
    - Locks application in portrait mode

---
+++ MusicSearchChallenge/MusicSearchChallengeTests/MusicSearchChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearchChallengeTests.swift
+//  MusicSearchChallengeTests
+//
+//  Created by Roger dos Santos Oliveira on 06/04/26.
+//
+
+import Testing
+@testable import MusicSearchChallenge
+
+struct MusicSearchChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
