13b891fd6f92d24be9f2b8e8dc299885f1648c3f
Roger Oliveira <rogersanoli.app@gmail.com>
2026-04-07 15:44:55 -0300

    [Challenge-002] Create Network package.
    This commit creates the initial structure for the local Network SPM package.
    
    - Creates the package
    - Links the package with the target
    - Updates .gitignore to include local packages' contents

---
+++ MusicSearchChallenge/Packages/Network/Tests/NetworkTests/NetworkTests.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkTests.swift
+//  MusicSearchChallengeTests
+//
+//  Created by Roger dos Santos Oliveira on 06/04/26.
+//
+
+
+import Testing
+@testable import Network
+
+struct NetworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
